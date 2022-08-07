775b78924b9d438465f3bb41281cb7c8f7a2d5ef
Linhai Song <songlinhai0543@gmail.com>
2022-08-07 18:40:23 +0800

    remove unnecessary files

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-cargo build
-rm -rf examples/intra/target
-cd examples/intra && RUSTFLAGS="--emit=mir" RUSTC="/Users/xsh/code/deadlock-lsp/target/debug/rustc" RUST_LOG=trace RUST_BACKTRACE=full __DL_OUT="/Users/xsh/code/deadlock-lsp/a.json" __DL_CRATE="intra" cargo build
